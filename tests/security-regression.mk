d5c0ed08b7bfd25c7df0388940b51c45dc57398e
John Reese <intel@swordintelligence.airforce>
2026-09-08 20:39:44 +0100

    test: aggregate QIHSE security regressions

--- tests/security-regression.mk
+++ tests/security-regression.mk
@@ -1,8 +1,18 @@
 # Security regression targets layered on top of the root Makefile.
-# Usage: make -f Makefile -f tests/security-regression.mk test-kv-security-regression
+# Usage: make -f Makefile -f tests/security-regression.mk test-security-regressions
 
-.PHONY: test-kv-security-regression
+.PHONY: test-kv-security-regression test-keystone-qihse-security test-vector-skip-integrity test-security-regressions
 
 test-kv-security-regression: lib
 	$(CC) $(CFLAGS) -o tests/test_kv_security_regression tests/test_kv_security_regression.c -L. -lqihse $(LDFLAGS)
 	LD_LIBRARY_PATH=. ./tests/test_kv_security_regression
+
+test-keystone-qihse-security: lib
+	$(CC) $(CFLAGS) -o tests/test_keystone_qihse_integration tests/test_keystone_qihse_integration.c -L. -lqihse $(LDFLAGS)
+	LD_LIBRARY_PATH=. ./tests/test_keystone_qihse_integration
+
+test-vector-skip-integrity: lib
+	$(CC) $(CFLAGS) -Ipersistence -o tests/test_vector_skip_integrity tests/test_vector_skip_integrity.c -L. -lqihse $(LDFLAGS)
+	LD_LIBRARY_PATH=. ./tests/test_vector_skip_integrity
+
+test-security-regressions: test-kv-security-regression test-keystone-qihse-security test-vector-skip-integrity
